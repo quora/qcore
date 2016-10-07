220857f2e292fdc83a7274ffd95f54676e1ebe0c
Manan Nayak <manan@quora.com>
2016-10-07 23:38:57 +0000

    version bump
    updating pypi package

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ CYTHON_MODULES = ['helpers', 'microtime', 'events', 'decorators', 'caching', 'in
 DATA_FILES = ['%s.pxd' % module for module in CYTHON_MODULES]
 
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 
 EXTENSIONS = [
