9f47c9240f9f05df12b4655cdbe46ba089b2b101
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-08-08 23:47:06 -0600

    add stubs for qcore

---
+++ stubs/qcore/__init__.pyi
@@ -0,0 +1,21 @@
+from . import inspection
+from .inspection import get_original_fn as get_original_fn, get_full_name as get_full_name
+from .errors import *
+from .helpers import *
+from .enum import *
+from .microtime import *
+from . import events
+from .events import (
+    EventHook as EventHook,
+    EventHub as EventHub,
+    EnumBasedEventHub as EnumBasedEventHub,
+    EventInterceptor as EventInterceptor,
+    sinking_event_hook as sinking_event_hook
+)
+from .decorators import *
+from .caching import *
+from . import debug
+from . import testing
+from . import asserts
+from .inspectable_class import InspectableClass as InspectableClass
+from .disallow_inheritance import DisallowInheritance as DisallowInheritance
