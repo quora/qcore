93c01944e24801d4f519c4c302b6601d809f1eb0
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-06-21 20:44:52 -0700

    put it in testpypi

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
         long_description = f.read()
 
     setup(
-        name="qcore",
+        name="qcorex",
         version=VERSION,
         author="Quora, Inc.",
         author_email="asynq@quora.com",
