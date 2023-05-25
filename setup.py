bc127aa5b5ee3f281672a7426310ba9812d95ba6
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-05-25 10:42:16 -0700

    Update dependencies and Python versions

--- setup.py
+++ setup.py
@@ -62,11 +62,11 @@ if __name__ == "__main__":
         classifiers=[
             "License :: OSI Approved :: Apache Software License",
             "Programming Language :: Python",
-            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
         ],
         keywords="quora core common utility",
         packages=["qcore", "qcore.tests"],
